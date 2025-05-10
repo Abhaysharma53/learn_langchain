6bfb5c2d872fbcb92ed583257d29c7ca5dc6ad83
Abhay <abhaysharma.cs2017.pgdav@gmail.com>
2025-05-11 01:01:16 +0530

    runnable sequence

---
+++ langchain_runnables/seuence_runnable.py
@@ -0,0 +1,19 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import  RunnableSequence
+from dotenv import load_dotenv
+
+
+model = ChatOpenAI()
+
+prompt = PromptTemplate(template= "Create a joke on the {topic}",
+                        input_variables= ['topic'])
+
+prompt2 = PromptTemplate(template= 'Explain the folowing joke - {text}',
+                         input_variables= ['text'])
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt, model, parser, prompt2, model, parser)
+print(chain.invoke({'topic':'LLM'}))
